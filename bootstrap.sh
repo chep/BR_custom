0ad4f07d181712c7ef200bf44b7dac2591f6f46e
Cédric Chépied <cedric.chepied@gmail.com>
2014-01-13 09:49:08 +0100

    bootstrap.sh: use git if user wants to
    If an argument is given to bootstrap.sh, it is used as a git commit
    hash. Buildroot is git cloned and the tree is reset to the given commit.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,41 +2,41 @@
 
 ####DO_NOT_TOUCH####
 
-VERSION=2013.11
-NAME=buildroot-${VERSION}.tar.bz2
-URL=http://buildroot.uclibc.org/downloads/${NAME}
 
-wget ${URL} -O /tmp/${NAME}
+# if an argument is given, we git clone buildroot and use the argument
+# as the commit where we want to go back in tree
+if [ $# = 0 ]
+then
+    VERSION=2013.11
+    NAME=buildroot-${VERSION}.tar.bz2
+    URL=http://buildroot.uclibc.org/downloads/${NAME}
 
-tar xvf /tmp/${NAME} -C . --strip 1
+    wget ${URL} -O /tmp/${NAME}
 
-cat >> .gitignore <<EOF
-/arch
-/Config.in
-/COPYING
-/linux
-/package
-/toolchain
-/board
-/Config.in.legacy
-/docs
-/Makefile
-/support
-/boot
-/CHANGES
-/configs
-/fs
-/Makefile.legacy
-/system
-/.defconfig
-/.gitignore
-/bootstrap.sh
-EOF
+    mkdir buildroot
+    cd buildroot
+    tar xvf /tmp/${NAME} -C . --strip 1
+else
+    git clone http://git.buildroot.net/git/buildroot.git buildroot
+    cd buildroot
+    git checkout $1
+fi
 
 # patch buildroot
-for p in custom/patchs/*.patch
+for p in ../custom/patchs/*.patch
 do
 	patch < $p
 done
 
+#back to root directory
+cd ..
+
+#gitignore
+cat >> .gitignore <<EOF
+/buildroot
+/.gitignore
+/bootstrap.sh
+EOF
+
+#dummyfy bootstrap.sh
 sed -i '0,/####DO_NOT_TOUCH####/s//echo "bootstrap.sh already done" ; exit 0/g' ./bootstrap.sh
